370b370e3d26601cfc16315337e1ca8821fffadd
MDPetrovich <greshnik578@yandex.ru>
2021-06-21 20:27:17 +0300

    init: do all tasks

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,35 @@
 package subtask1
 
-class DateFormatter {
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
-    // TODO: Complete the following function
+class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val simpleDateFormatValidator = SimpleDateFormat("dd.MM.yyyy", Locale("ru"))
+        val simpleDateFormatResult = SimpleDateFormat("dd MMMM, EEEE", Locale("ru"))
+        val dateResult: Date
+
+        simpleDateFormatValidator.isLenient = false
+        try {
+            dateResult = simpleDateFormatValidator.parse("${day.toString()}.${month.toString()}.${year.toString()}")
+        } catch ( e: ParseException ) {
+            return "Такого дня не существует"
+        }
+        return simpleDateFormatResult.format(dateResult)
     }
+}
+
+fun main(args: Array<String>) {
+    val date = Date()
+    val calendar = Calendar.getInstance()
+
+    calendar.time = date
+    val dateFormat = DateFormat.getDateInstance(DateFormat.SHORT)
+
+    val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+
+    val newDate = simpleDateFormat.parse("05/12/1985")
+    println(newDate)
 }
